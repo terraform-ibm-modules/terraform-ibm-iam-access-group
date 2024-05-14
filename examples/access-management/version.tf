0b71409eb18c39904caafc551e44890c48b02710
Matthew.Lemmond@ibm.com <Matthew.Lemmond@ibm.com>
2024-05-14 16:00:39 -0400

    refactor: change cra target, restrict version.tf

--- examples/access-management/version.tf
+++ examples/access-management/version.tf
@@ -1,10 +1,9 @@
 terraform {
   required_version = ">= 1.3.0, <1.7.0"
   required_providers {
-    # Lock DA into an exact provider version - renovate automation will keep it updated
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = ">= 1.51.0"
+      version = ">= 1.51.0, < 2.0.0"
     }
   }
 }
