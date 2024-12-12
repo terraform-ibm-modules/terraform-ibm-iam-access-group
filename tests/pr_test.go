5af8efd3b07517cafeecab7eab8e391786e76e3c
Matthew.Lemmond@ibm.com <Matthew.Lemmond@ibm.com>
2024-12-12 12:02:56 -0500

    refactor: fix solution dir test is pointing to

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -14,7 +14,7 @@ import (
 const resourceGroup = "geretain-test-resources"
 const basicExampleDir = "examples/basic"
 const moduleExampleDir = "examples/access-management"
-const solutionDir = "solutions/access-management"
+const solutionDir = "solutions/standard"
 
 func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
 	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
