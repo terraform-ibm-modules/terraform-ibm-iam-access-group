71328d142f2940bb3d7f2d831fa5410af036005b
Matthew Lemmond <Matthew.Lemmond@ibm.com>
2024-12-13 11:24:16 -0500

    feat: added new DA for IBM catalog (#233)

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,16 +2,19 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testschematic"
 )
 
 // Use existing resource group
 const resourceGroup = "geretain-test-resources"
 const basicExampleDir = "examples/basic"
 const moduleExampleDir = "examples/access-management"
+const solutionDir = "solutions/standard"
 
 func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
 	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
@@ -65,6 +68,54 @@ func TestRunModuleUpgrade(t *testing.T) {
 	t.Parallel()
 
 	options := setupModuleOptions(t, "ag-upg-module", moduleExampleDir)
+
+	output, err := options.RunTestUpgrade()
+	if !options.UpgradeTestSkipped {
+		assert.Nil(t, err, "This should not have errored")
+		assert.NotNil(t, output, "Expected some output")
+	}
+}
+
+func setupDAOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
+	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
+		Testing:      t,
+		TerraformDir: dir,
+		Prefix:       prefix,
+	})
+
+	options.TerraformVars = map[string]interface{}{
+		"provider_visibility": "public",
+		"prefix":              options.Prefix,
+	}
+
+	return options
+}
+
+func TestRunDASchematics(t *testing.T) {
+	options := testschematic.TestSchematicOptionsDefault(&testschematic.TestSchematicOptions{
+		Testing:                t,
+		TarIncludePatterns:     []string{"*.tf", fmt.Sprintf("%s/*.tf", solutionDir), "modules/access-management/*.tf"},
+		TemplateFolder:         solutionDir,
+		ResourceGroup:          resourceGroup,
+		Prefix:                 "acc-mgmt",
+		Tags:                   []string{"test-schematic"},
+		DeleteWorkspaceOnFail:  false,
+		WaitJobCompleteMinutes: 60,
+	})
+
+	options.TerraformVars = []testschematic.TestSchematicTerraformVar{
+		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
+		{Name: "prefix", Value: options.Prefix, DataType: "string"},
+	}
+
+	err := options.RunSchematicTest()
+	assert.NoError(t, err, "Schematic Test had unexpected error")
+}
+
+func TestRunUpgradeDA(t *testing.T) {
+	t.Parallel()
+
+	options := setupDAOptions(t, "acc-mgmt-upg", solutionDir)
 
 	output, err := options.RunTestUpgrade()
 	if !options.UpgradeTestSkipped {
