81878931ac9be64d088f2ee1e19ae443311847ea
iamar7 <iamar7@users.noreply.github.com>
2026-05-05 12:42:06 +0530

    fix: standardize resource_tags variable, update descriptions and add access tag validation

--- solutions/standard/main.tf
+++ solutions/standard/main.tf
@@ -7,7 +7,7 @@ module "access_groups" {
   admin_observability_ag_ibm_ids             = var.admin_observability_ag_ibm_ids
   admin_observability_ag_service_ids         = var.admin_observability_ag_service_ids
   admin_observability_ag_trusted_profile_ids = var.admin_observability_ag_trusted_profile_ids
-  admin_observability_ag_tags                = var.admin_observability_ag_tags
+  resource_tags = var.admin_observability_ag_tags
 
   admin_security_ag_name                = try("${var.prefix}-${var.admin_security_ag_name}", var.admin_security_ag_name)
   admin_security_ag_description         = replace(format("Created by terraform-ibm-iam-access-group deployable architecture. %v", var.admin_security_ag_description), " null", "")
@@ -15,7 +15,7 @@ module "access_groups" {
   admin_security_ag_ibm_ids             = var.admin_security_ag_ibm_ids
   admin_security_ag_service_ids         = var.admin_security_ag_service_ids
   admin_security_ag_trusted_profile_ids = var.admin_security_ag_trusted_profile_ids
-  admin_security_ag_tags                = var.admin_security_ag_tags
+  resource_tags = var.admin_security_ag_tags
 
   admin_network_ag_name                = try("${var.prefix}-${var.admin_network_ag_name}", var.admin_network_ag_name)
   admin_network_ag_description         = replace(format("Created by terraform-ibm-iam-access-group deployable architecture. %v", var.admin_network_ag_description), " null", "")
@@ -23,7 +23,7 @@ module "access_groups" {
   admin_network_ag_ibm_ids             = var.admin_network_ag_ibm_ids
   admin_network_ag_service_ids         = var.admin_network_ag_service_ids
   admin_network_ag_trusted_profile_ids = var.admin_network_ag_trusted_profile_ids
-  admin_network_ag_tags                = var.admin_network_ag_tags
+  resource_tags = var.admin_network_ag_tags
 
   admin_compute_ag_name                = try("${var.prefix}-${var.admin_compute_ag_name}", var.admin_compute_ag_name)
   admin_compute_ag_description         = replace(format("Created by terraform-ibm-iam-access-group deployable architecture. %v", var.admin_compute_ag_description), " null", "")
@@ -31,7 +31,7 @@ module "access_groups" {
   admin_compute_ag_ibm_ids             = var.admin_compute_ag_ibm_ids
   admin_compute_ag_service_ids         = var.admin_compute_ag_service_ids
   admin_compute_ag_trusted_profile_ids = var.admin_compute_ag_trusted_profile_ids
-  admin_compute_ag_tags                = var.admin_compute_ag_tags
+  resource_tags = var.admin_compute_ag_tags
 
   privileged_observability_ag_name                = try("${var.prefix}-${var.privileged_observability_ag_name}", var.privileged_observability_ag_name)
   privileged_observability_ag_description         = replace(format("Created by terraform-ibm-iam-access-group deployable architecture. %v", var.privileged_observability_ag_description), " null", "")
@@ -39,7 +39,7 @@ module "access_groups" {
   privileged_observability_ag_ibm_ids             = var.privileged_observability_ag_ibm_ids
   privileged_observability_ag_service_ids         = var.privileged_observability_ag_service_ids
   privileged_observability_ag_trusted_profile_ids = var.privileged_observability_ag_trusted_profile_ids
-  privileged_observability_ag_tags                = var.privileged_observability_ag_tags
+  resource_tags = var.privileged_observability_ag_tags
 
   privileged_security_ag_name                = try("${var.prefix}-${var.privileged_security_ag_name}", var.privileged_security_ag_name)
   privileged_security_ag_description         = replace(format("Created by terraform-ibm-iam-access-group deployable architecture. %v", var.privileged_security_ag_description), " null", "")
@@ -47,7 +47,7 @@ module "access_groups" {
   privileged_security_ag_ibm_ids             = var.privileged_security_ag_ibm_ids
   privileged_security_ag_service_ids         = var.privileged_security_ag_service_ids
   privileged_security_ag_trusted_profile_ids = var.privileged_security_ag_trusted_profile_ids
-  privileged_security_ag_tags                = var.privileged_security_ag_tags
+  resource_tags = var.privileged_security_ag_tags
 
   privileged_network_ag_name                = try("${var.prefix}-${var.privileged_network_ag_name}", var.privileged_network_ag_name)
   privileged_network_ag_description         = replace(format("Created by terraform-ibm-iam-access-group deployable architecture. %v", var.privileged_network_ag_description), " null", "")
@@ -55,7 +55,7 @@ module "access_groups" {
   privileged_network_ag_ibm_ids             = var.privileged_network_ag_ibm_ids
   privileged_network_ag_service_ids         = var.privileged_network_ag_service_ids
   privileged_network_ag_trusted_profile_ids = var.privileged_network_ag_trusted_profile_ids
-  privileged_network_ag_tags                = var.privileged_network_ag_tags
+  resource_tags = var.privileged_network_ag_tags
 
   privileged_compute_ag_name                = try("${var.prefix}-${var.privileged_compute_ag_name}", var.privileged_compute_ag_name)
   privileged_compute_ag_description         = replace(format("Created by terraform-ibm-iam-access-group deployable architecture. %v", var.privileged_compute_ag_description), " null", "")
@@ -63,7 +63,7 @@ module "access_groups" {
   privileged_compute_ag_ibm_ids             = var.privileged_compute_ag_ibm_ids
   privileged_compute_ag_service_ids         = var.privileged_compute_ag_service_ids
   privileged_compute_ag_trusted_profile_ids = var.privileged_compute_ag_trusted_profile_ids
-  privileged_compute_ag_tags                = var.privileged_compute_ag_tags
+  resource_tags = var.privileged_compute_ag_tags
 
   observer_observability_ag_name                = try("${var.prefix}-${var.observer_observability_ag_name}", var.observer_observability_ag_name)
   observer_observability_ag_description         = replace(format("Created by terraform-ibm-iam-access-group deployable architecture. %v", var.observer_observability_ag_description), " null", "")
@@ -71,7 +71,7 @@ module "access_groups" {
   observer_observability_ag_ibm_ids             = var.observer_observability_ag_ibm_ids
   observer_observability_ag_service_ids         = var.observer_observability_ag_service_ids
   observer_observability_ag_trusted_profile_ids = var.observer_observability_ag_trusted_profile_ids
-  observer_observability_ag_tags                = var.observer_observability_ag_tags
+  resource_tags = var.observer_observability_ag_tags
 
   observer_security_ag_name                = try("${var.prefix}-${var.observer_security_ag_name}", var.observer_security_ag_name)
   observer_security_ag_description         = replace(format("Created by terraform-ibm-iam-access-group deployable architecture. %v", var.observer_security_ag_description), " null", "")
@@ -79,7 +79,7 @@ module "access_groups" {
   observer_security_ag_ibm_ids             = var.observer_security_ag_ibm_ids
   observer_security_ag_service_ids         = var.observer_security_ag_service_ids
   observer_security_ag_trusted_profile_ids = var.observer_security_ag_trusted_profile_ids
-  observer_security_ag_tags                = var.observer_security_ag_tags
+  resource_tags = var.observer_security_ag_tags
 
   observer_network_ag_name                = try("${var.prefix}-${var.observer_network_ag_name}", var.observer_network_ag_name)
   observer_network_ag_description         = replace(format("Created by terraform-ibm-iam-access-group deployable architecture. %v", var.observer_network_ag_description), " null", "")
@@ -87,7 +87,7 @@ module "access_groups" {
   observer_network_ag_ibm_ids             = var.observer_network_ag_ibm_ids
   observer_network_ag_service_ids         = var.observer_network_ag_service_ids
   observer_network_ag_trusted_profile_ids = var.observer_network_ag_trusted_profile_ids
-  observer_network_ag_tags                = var.observer_network_ag_tags
+  resource_tags = var.observer_network_ag_tags
 
   observer_compute_ag_name                = try("${var.prefix}-${var.observer_compute_ag_name}", var.observer_compute_ag_name)
   observer_compute_ag_description         = replace(format("Created by terraform-ibm-iam-access-group deployable architecture. %v", var.observer_compute_ag_description), " null", "")
@@ -95,5 +95,5 @@ module "access_groups" {
   observer_compute_ag_ibm_ids             = var.observer_compute_ag_ibm_ids
   observer_compute_ag_service_ids         = var.observer_compute_ag_service_ids
   observer_compute_ag_trusted_profile_ids = var.observer_compute_ag_trusted_profile_ids
-  observer_compute_ag_tags                = var.observer_compute_ag_tags
+  resource_tags = var.observer_compute_ag_tags
 }
